43a8a0400262d2b776811f72270fd347104f1a03
alfiooatmaja <alfiooatmaja@gmail.com>
2023-11-04 17:02:11 +0700

    membuat_tampilan_backend

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('pages.blank-page', ['type_menu' => '']);
+    return view('auth,login', ['type_menu' => '']);
    });
+
+//Route::get('/login', function () {
+    // return view('welcome');
+    return view('auth.login' );
+//   });
+
+//Route::get('/register', function () {
+//    return view('auth.register' );
+//   });
    
+//Route::get('/reset', function () {
+//    return view('auth.reset' );
+//  });
+
+//Route::get('/forgot', function () {
+//    return view('auth.forgot' );
+//   });
+
+//Route::get('/verify', function () {
+//    return view('auth.verify' );
+//   });
