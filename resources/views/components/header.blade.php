68dbcbe6ce1a91bfe29ff88eb9e90295d2bb35ec
alfiooatmaja <alfiooatmaja@gmail.com>
2023-11-04 21:14:01 +0700

    membuat_logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -101,24 +101,21 @@
                 <div class="d-sm-none d-lg-inline-block">Hi, {{ auth()->user()->name }}</div>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
-                <div class="dropdown-title">Logged in 5 min ago</div>
+            
                 <a href="features-profile.html"
                     class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>
-                <a href="features-activities.html"
-                    class="dropdown-item has-icon">
-                    <i class="fas fa-bolt"></i> Activities
-                </a>
-                <a href="features-settings.html"
-                    class="dropdown-item has-icon">
-                    <i class="fas fa-cog"></i> Settings
-                </a>
+                
                 <div class="dropdown-divider"></div>
                 <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                    class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id='logout-form' action="{{ route('logout') }}" method="post">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
