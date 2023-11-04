f11137e9a8778b4a8ca35cea7e2a2c0df3b57e6f
alfiooatmaja <alfiooatmaja@gmail.com>
2023-11-04 20:42:37 +0700

    Auth_Fortify

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('auth,login', ['type_menu' => '']);
+    return view('auth.login', ['type_menu' => '']);
    });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function(){
+        return   view('pages.blank-page',['type_menu' => '']);
+    })->name('home');
+});
 
 //Route::get('/login', function () {
     // return view('welcome');
-    return view('auth.login' );
+//    return view('auth.login' );
 //   });
 
 //Route::get('/register', function () {
