67e51d32c88d26aac3ade46349ae020b78883af7
alfiooatmaja <alfiooatmaja@gmail.com>
2024-01-08 00:03:15 +0700

    fcm integration

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,9 +7,29 @@ use App\Models\Order;
 use App\Models\OrderItem;
 use App\Services\Midtrans\CreatePaymentUrlService;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class OrderController extends Controller
 {
+    public function sendNotificationToUser($userId, $message)
+    {
+        // Dapatkan FCM token user dari tabel 'users'
+
+        $user = User::find($userId);
+        $token = $user->fcm_token;
+
+        // Kirim notifikasi ke perangkat Android
+        $messaging = app('firebase.messaging');
+        $notification = Notification::create('Order Masuk', $message);
+
+        $message = CloudMessage::withTarget('token', $token)
+            ->withNotification($notification);
+
+        $messaging->send($message);
+    }
+
     public function order(Request $request)
     {
         $order = Order::create([
@@ -29,6 +49,7 @@ class OrderController extends Controller
             ]);
         }
 
+        $this->sendNotificationToUser($request->seller_id, 'Order ' . $request->total_price . ' Masuk, Menunggu Pembayaran');
         $midtrans = new CreatePaymentUrlService();
         $paymentUrl = $midtrans->getPaymentUrl($order->load('user', 'orderItems'));
 
